be6586efe44119fa4ffe86c8b48fe7a4a68d9772
RubnK <40402492+RubnK@users.noreply.github.com>
2025-03-18 17:32:28 +0100

    Renommer le projet en RentACar, mettre à jour la version de Java et ajouter la classe Car avec un service REST pour gérer les voitures

--- src/main/java/com/rubnk/RentACar/RentACarApplication.java
+++ src/main/java/com/rubnk/RentACar/RentACarApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.rubnk.RentACar;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class RentACarApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(RentACarApplication.class, args);
 	}
 
 }
